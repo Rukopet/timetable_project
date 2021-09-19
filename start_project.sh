3f9533f7b1438942c316bb4fcff5fb987031f0df
egums <dima110.ru@gmail.com>
2021-09-19 21:28:18 +0300

    last commit

---
+++ start_project.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+FOLDER_WITH_SCRIPTS="core_scripts"
+
+# Весь вывод дублируется в файл
+LOG_LOCATION=$SCRIPTPATH/.default/log
+exec > >(tee $LOG_LOCATION) 2>&1
+
+# Подгружаем настройки переменных
+unamestr=$(uname)
+if [ "$unamestr" = 'Linux' ]; then
+
+  export $(grep -v '^#' $SCRIPTPATH/.default/.env | xargs -d '\n')
+  export $(grep -v '^#' $SCRIPTPATH/.env | xargs -d '\n')
+
+
+elif [ "$unamestr" = 'Darwin' ]; then
+
+  export $(grep -v '^#' $SCRIPTPATH/.default/.env | xargs -I '\n')
+  export $(grep -v '^#' $SCRIPTPATH/.env | xargs -I '\n')
+
+elif [ "$unamestr" = 'FreeBSD' ]; then
+
+  export $(grep -v '^#' $SCRIPTPATH/.default/.env | xargs -0)
+  export $(grep -v '^#' $SCRIPTPATH/.env | xargs -0)
+
+fi
+
+compose_start() {
+  docker-compose -f  docker-compose.yaml up
+}
+
+restart() {
+
+}
+
+code_reload() {
+  docker-compose up --detach --build
+}
+
+if [ $1 = 'reload' ]; then
+  code_reload
+fi
